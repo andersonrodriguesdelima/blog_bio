128478c18fb6bc16600458b4c2c5b377393916ae
Anderson Rodrigues de Lima <anderson.rodrigues.lima@hotmail.com>
2017-05-18 14:53:43 -0300

    criando tela de visualização do post

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,4 +2,8 @@ class IndexController < ApplicationController
 	def home
 		@posts = Post.all
 	end
+
+	def show_post
+		@post = Post.find(params[:id])
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :posts
 	root 'index#home'
+	get '/show_post/:id' => 'index#show_post'
 end
