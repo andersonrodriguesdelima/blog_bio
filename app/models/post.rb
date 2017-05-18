12a086fce89dda02d36da7ce8fbb6e7013eafcc4
Anderson Rodrigues de Lima <anderson.rodrigues.lima@hotmail.com>
2017-05-18 11:30:46 -0300

    removendo do save para manter texto com markdown no editar

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,2 @@
 class Post < ApplicationRecord
-	before_create do
-		self.titulo   = markdown(self.titulo)
-		self.conteudo = markdown(self.conteudo)
-	end
-
-	before_update do
-		self.titulo   = markdown(self.titulo)
-		self.conteudo = markdown(self.conteudo)
-	end
 end
