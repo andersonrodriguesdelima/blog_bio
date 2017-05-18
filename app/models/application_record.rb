e9c1bb1a711e82084f21839368cb080fc9d375d1
Anderson Rodrigues de Lima <anderson.rodrigues.lima@hotmail.com>
2017-05-18 11:30:20 -0300

    removendo do model para manter texto com markdown no editar

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,24 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
 	self.abstract_class = true
-
-	def markdown(text)
-		options = {
-			filter_html:     true,
-			hard_wrap:       true,
-			link_attributes: { rel: 'nofollow', target: "_blank" },
-			space_after_headers: true,
-			fenced_code_blocks: true
-		}
-
-		extensions = {
-			autolink:           true,
-			superscript:        true,
-			disable_indented_code_blocks: true
-		}
-
-		renderer = Redcarpet::Render::HTML.new(options)
-		markdown = Redcarpet::Markdown.new(renderer, extensions)
-		m = Redcarpet::Markdown.new(renderer)
-		m.render(text).html_safe
-	end
 end
