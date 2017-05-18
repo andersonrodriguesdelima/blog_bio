18f730595396167d1c1e72fc630981a9c50abb25
Anderson Rodrigues de Lima <anderson.rodrigues.lima@hotmail.com>
2017-05-18 10:27:22 -0300

    usando RedCarpet para converter em markdown

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+	before_create do
+		self.titulo   = markdown(self.titulo)
+		self.conteudo = markdown(self.conteudo)
+	end
+
+	before_update do
+		self.titulo   = markdown(self.titulo)
+		self.conteudo = markdown(self.conteudo)
+	end
 end
