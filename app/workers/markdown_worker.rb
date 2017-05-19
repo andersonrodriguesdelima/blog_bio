0fed1b0d0cd16c8f3f0b07c93607037e5c673b8d
Anderson Rodrigues de Lima <anderson.rodrigues.lima@hotmail.com>
2017-05-19 16:25:46 -0300

    worker com conversão

---
+++ app/workers/markdown_worker.rb
@@ -0,0 +1,27 @@
+class MarkdownWorker
+	include Sidekiq::Worker
+
+	def perform(post_id, post_titulo, post_conteudo)
+		options = {
+			filter_html:     true,
+			hard_wrap:       true,
+			link_attributes: { rel: 'nofollow', target: "_blank" },
+			space_after_headers: true,
+			fenced_code_blocks: true
+		}
+
+		extensions = {
+			autolink:           true,
+			superscript:        true,
+			disable_indented_code_blocks: true
+		}
+		post = Post.find(post_id)
+
+		renderer = Redcarpet::Render::HTML.new(options)
+		markdown = Redcarpet::Markdown.new(renderer, extensions)
+		m = Redcarpet::Markdown.new(renderer)
+		post.titulo = m.render(post_titulo)
+		post.conteudo = m.render(post_conteudo)
+		post.save!
+	end
+end
