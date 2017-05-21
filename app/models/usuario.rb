c8122bb212417f7a7d1e84a037d364724a85e21e
Anderson Rodrigues de Lima <anderson.rodrigues.lima@hotmail.com>
2017-05-21 17:10:30 -0300

    spec usuarios e validação

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -5,7 +5,9 @@ class Usuario < ApplicationRecord
 	validates_presence_of :nome, length: {maximum: 50}, :message => "não pode ser vazio. Máximo de 50 letras."
   	validates_presence_of :password, length: {minimum: 6}, :message => "não pode ser vazio. Minimo de 6 caracteres."
   	VALID_EMAIL_FORMAT= /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-  	validates_presence_of :email, length: {maximum: 260}, format: { with: VALID_EMAIL_FORMAT}, uniqueness: {case_sensitive: false}, :message => "não pode ser vazio."
+  	validates_presence_of :email, length: {maximum: 260}, :message => "não pode ser vazio."
+  	validates :email, format: { with: VALID_EMAIL_FORMAT, message: "inválido. Digite um e-mail válido." }
+  	validates :email, uniqueness: {case_sensitive: false, :message => "já está em uso. Favor registrar outro e-mail."}
   	
   	before_save do
   		self.email = email.downcase
--- spec/models/usuario_spec.rb
+++ spec/models/usuario_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Usuario, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it 'requer nome do usuario' do
+		@user = Usuario.new(:nome => nil)
+		expect(@user).to_not be_valid
+		expect(@user.errors[:nome]).to include("não pode ser vazio. Máximo de 50 letras.")
+	end
+	it 'requer password do usuario' do
+		@user = Usuario.new(:password => nil)
+		expect(@user).to_not be_valid
+		expect(@user.errors[:password]).to include("não pode ser vazio. Minimo de 6 caracteres.")
+	end
+	it 'requer email do usuario' do
+		@user = Usuario.new(:email => nil)
+		expect(@user).to_not be_valid
+		expect(@user.errors[:email]).to include("não pode ser vazio.")
+	end
+	it 'requer que email siga a formatação' do
+		@user = Usuario.new(:email => "andersonrodriguesdelima")
+		expect(@user).to_not be_valid
+		expect(@user.errors[:email]).to include("inválido. Digite um e-mail válido.")
+	end
+	it 'Deve haver somente um usuario por email' do
+		@user = Usuario.new(
+			:nome => "Anderson",
+			:email => "anderson@bio.com.br",
+			:password => "123456",
+			:password_confirmation => "123456"
+			)
+		@user.save!
+		@user_error = Usuario.new(
+			:nome => "Anderson",
+			:email => "anderson@bio.com.br",
+			:password => "123456",
+			:password_confirmation => "123456"
+		)
+		expect(@user_error.save).to be false
+		expect(@user_error.errors[:email]).to include("já está em uso. Favor registrar outro e-mail.")
+	end
+	it 'Deve colocar email em downcase' do
+		@user = Usuario.new(
+			:nome => "Anderson",
+			:email => "ANDERSON@BIO.COM.BR",
+			:password => "123456",
+			:password_confirmation => "123456"
+			)
+		expect(@user.save!).to be true
+		expect(@user.email).to eq("anderson@bio.com.br")
+	end
 end
