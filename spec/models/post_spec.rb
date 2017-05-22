7cdfe94d2e350aee54b0ab8510f2a5cb0d9a4cb7
Anderson Rodrigues de Lima <anderson.rodrigues.lima@hotmail.com>
2017-05-22 09:41:58 -0300

    ajuste spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,17 +4,19 @@ RSpec.describe Post, type: :model do
 	it 'Requer titulo para o post' do
 		@post = Post.new(:titulo => nil)
 		expect(@post).to_not be_valid
-		@post.errors[:titulo].first.eql? "não pode ser vazio"
+		expect(@post.errors[:titulo]).to include("não pode ser vazio")
 	end
 
 	it 'Requer conteudo para o post' do
 		@post = Post.new(:conteudo => nil)
 		expect(@post).to_not be_valid
-		@post.errors[:conteudo].first.eql? "não pode ser vazio"
+		expect(@post.errors[:conteudo]).to include("não pode ser vazio")
 	end
 
 	it 'Deve processar o conteudo em markdown e retornar em html' do
-		@post = Post.new(:titulo => "Links", :conteudo => "Esses são os *links*")
+		@user = Usuario.new(:nome => "anderson", :email => "anderson@bio.com.br", :password => "123456", :password_confirmation => "123456")
+		@user.save!
+		@post = Post.new(:titulo => "Links", :conteudo => "Esses são os *links*", :usuario_id => @user.id)
 		@post.save!
 		expect(@post.conteudo).to eq("Esses são os *links*")
 	end
